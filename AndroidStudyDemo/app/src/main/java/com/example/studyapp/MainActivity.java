aacef44403de717dda23d4a5130954a12514cac0
zhaolong <928343994@qq.com>
2021-09-08 21:31:54 +0800

    【11 】

---
+++ AndroidStudyDemo/app/src/main/java/com/example/studyapp/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.studyapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+findViewById()
+
+    }
+}
